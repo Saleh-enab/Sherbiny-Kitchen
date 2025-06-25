0421556b64e2490709c78aafdee2cce6cc804ede
Saleh <salehenab850@gmail.com>
2025-06-26 00:09:02 +0300

    feat: add user registration schema and validation logic

---
+++ packages/api/src/schemas/userRegister.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+import { validate } from '../utils/validator.js';
+
+export const userSchema = z.object({
+    body: z.object({
+        name: z.string(),
+        email: z.string().email(),
+        password: z.string(),
+        confirmPassword: z.string()
+    })
+
+}).refine((data) => data.body.password === data.body.confirmPassword, {
+    message: "Passwords don't match",
+    path: ["confirm"]
+})
+
+export const signUpValidator = validate(userSchema)
+export type SignUpMiddleware = typeof signUpValidator
