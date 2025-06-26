4a133035b994db44b7c5e7db84ec82aad211918d
Saleh <salehenab850@gmail.com>
2025-06-26 17:08:12 +0300

    refactor: update ESLint configuration to use TypeScript parser and improve rule settings

--- packages/api/eslint.config.js
+++ packages/api/eslint.config.js
@@ -1,11 +1,32 @@
-import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
 
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-]);
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    languageOptions: {
+      globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: true,
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tseslint.plugin,
+    },
+    rules: {
+      "no-unused-vars": "off", // Turn off base rule
+      "@typescript-eslint/no-unused-vars": "error", // Use TypeScript-specific rule instead
+      "prefer-const": [
+        "error",
+        {
+          ignoreReadBeforeAssign: true,
+        },
+      ],
+    },
+  },
+  // ...tseslint.configs.recommended,
+];
