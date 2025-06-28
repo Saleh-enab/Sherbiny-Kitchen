05538fb4d778ffa3d9fb1b5d0ba376554e57f022
Saleh <salehenab850@gmail.com>
2025-06-28 19:48:31 +0300

    feat: add Redis service for caching recipes with set, get, and count functionalities

---
+++ packages/api/src/services/redis.service.ts
@@ -0,0 +1,78 @@
+import { redis } from "../db.js";
+import AppError from "../models/appError.js";
+import { IRecipe } from "../types/recipeType.js";
+import { logger } from "../utils/logger.js";
+
+export const setRecipe = async (key: string, data: IRecipe) => {
+    const oldRecipes = await redis.get(key);
+
+    if (oldRecipes) {
+        logger.info("Cache hit!, This recipe is already exits");
+        return;
+    }
+
+    const requestPayload = {
+        recipe: data,
+        requestedAt: new Date().toISOString()
+    };
+
+    const recipeJson = JSON.stringify(requestPayload);
+    const expireAt = new Date(Date.now() + 24 * 60 * 60 * 1000); // 1 day from now
+    const ttlSeconds = Math.floor((expireAt.getTime() - Date.now()) / 1000);
+
+    await redis.setex(key, ttlSeconds, recipeJson, (err) => {
+        if (err) {
+            throw AppError.custom(500, 1999, err.message)
+        } else {
+            logger.info("Recipe saved in cache successfully");
+        }
+    });
+}
+
+export const getRecipe = async (key: string) => {
+    const cachedData = await redis.get(key);
+    if (!cachedData) {
+        return null;
+    }
+    const parsed = JSON.parse(cachedData);
+    const cachedRecipe: IRecipe = parsed.recipe;
+    return cachedRecipe;
+}
+
+export const getTodayCachedRecipeCount = async () => {
+    const keys = await redis.keys("*");
+
+    if (!keys || keys.length === 0) {
+        console.log("No Keys");
+
+        return 0;
+    }
+
+    let count = 0;
+
+    for (const key of keys) {
+        const value = await redis.get(key);
+
+        if (value) {
+            const { requestedAt } = JSON.parse(value);
+
+            if (isToday(requestedAt)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
+};
+
+// Check if ISO string date is from today
+const isToday = (isoString: string) => {
+    const today = new Date();
+    const date = new Date(isoString);
+
+    return (
+        date.getFullYear() === today.getFullYear() &&
+        date.getMonth() === today.getMonth() &&
+        date.getDate() === today.getDate()
+    );
+};
