cf8214e79231977dd0796e0626e2a4b5f61b760e
Saleh <salehenab850@gmail.com>
2025-06-28 19:49:32 +0300

    feat: update recipe routes to include all CRUD operations and user authorization

--- packages/api/src/modules/recipe/recipe.route.ts
+++ packages/api/src/modules/recipe/recipe.route.ts
@@ -1,6 +1,10 @@
 import { Router } from "express";
-import { generateRecipe } from "./recipe.controller.js";
+import { generateRecipe, getAllRecipes, getRecipeData, saveRecipe } from "./recipe.controller.js";
+import { authorizeUser } from "../../middlewares/authorizeUser.js";
 
 export const recipeRouter = Router();
 
-recipeRouter.get('/generate', generateRecipe);
+recipeRouter.post('/generate', generateRecipe);
+recipeRouter.post('/save/:recipeKey', authorizeUser, saveRecipe);
+recipeRouter.get('/:recipeKey', authorizeUser, getRecipeData);
+recipeRouter.get('/', authorizeUser, getAllRecipes);
