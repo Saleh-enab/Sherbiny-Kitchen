93f60bbca0fc9e47cf4f09fb41f18d40ac0a5b26
Saleh <salehenab850@gmail.com>
2025-06-29 17:05:51 +0300

    fix: add user authorization to recipe generation route

--- packages/api/src/modules/recipe/recipe.route.ts
+++ packages/api/src/modules/recipe/recipe.route.ts
@@ -4,7 +4,7 @@ import { authorizeUser } from "../../middlewares/authorizeUser.js";
 
 export const recipeRouter = Router();
 
-recipeRouter.post('/generate', generateRecipe);
+recipeRouter.post('/generate', authorizeUser, generateRecipe);
 recipeRouter.post('/save/:recipeKey', authorizeUser, saveRecipe);
 recipeRouter.get('/:recipeKey', authorizeUser, getRecipeData);
 recipeRouter.get('/', authorizeUser, getAllRecipes);
